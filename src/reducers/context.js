641e8a06de552c5a2d801174713ce5ce873bc624
LUIZ FLAVIO RODRIGUES LOPES <luizflaviolopes@gmail.com>
2020-08-22 15:57:34 -0300

    Drag and Drop OK

--- src/reducers/context.js
+++ src/reducers/context.js
@@ -2,7 +2,11 @@ import * as actionTypes from "../types/actionTypes";
 import * as boardViewResolver from "../resolvers/boardViewResolver";
 import { getPositionBoardRelative } from "../helpers/getPositionBoardRelative";
 
-const setState = () => ({});
+const setState = () => ({
+  renderOriginalOrder: [],
+  renderSelecteds: [],
+  renderUnselecteds: [],
+});
 
 export default (state = setState(), action = {}) => {
   switch (action.type) {
